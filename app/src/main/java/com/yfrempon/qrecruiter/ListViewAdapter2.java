4ffe48a4c5fae30b50727d4b49594617e5e97ff8
Yaw Frempong <yfrempon@uncc.edu>
2020-09-07 19:45:28 -0400

    Update ListViewAdapter2.java

--- app/src/main/java/com/yfrempon/qrecruiter/ListViewAdapter2.java
+++ app/src/main/java/com/yfrempon/qrecruiter/ListViewAdapter2.java
@@ -28,16 +28,18 @@ import static com.yfrempon.qrecruiter.EventActivity.deleteSelected;
 import static java.lang.Character.isWhitespace;
 
 public class ListViewAdapter2 extends ArrayAdapter<String> {
-    private List<String> labels;
-    private List<String> labelsFull;
-    private Context context;
-    private String m_Text = "";
     public CheckBox checkBox;
     public ImageView img_star;
     public ImageView img_edit;
     public Map<String, String> favList = new HashMap<>();
     private Map<String, Object> results = new HashMap<>();
-
+    
+    private List<String> labels;
+    private List<String> labelsFull;
+    private Context context;
+    private String m_Text = "";
+    
+    //constructor(load profile names and favorite status)
     public ListViewAdapter2(List<String> labels, Context context, Map<String, String> favList){
         super(context, R.layout.row_item_2, labels);
         this.context = context;
@@ -45,33 +47,41 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
         labelsFull = new ArrayList<>(labels);
         this.favList.putAll(favList);
     }
-
+    
+    //for every list element set the label name and favorite star
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
         final int position_star = position;
         LayoutInflater inflater = ((Activity)context).getLayoutInflater();
         View row = inflater.inflate(R.layout.row_item_2,parent, false);
+        
+        //store activity content in variables
+        //set labels, checkbox location, and favorite status
         TextView label = row.findViewById(R.id.txtName);
         label.setText(labels.get(position));
-
         checkBox = row.findViewById(R.id.checkBox);
         checkBox.setTag(position);
         img_star = row.findViewById(R.id.favorite);
         img_star.setTag(position);
         img_edit = row.findViewById(R.id.editButton_2);
         img_edit.setTag(position);
+        
+        //toggle star filled/not filled based on favorite status
         if(favList.get(labels.get(position)).equals("t")){
             img_star.setImageResource(R.drawable.ic_star_filled);
         } else {
             img_star.setImageResource(R.drawable.ic_star_not_filled);
         }
+        
+        //toggle checkbox based on selected items
         if(EventActivity.userSelection.contains(labels.get(position))){
             checkBox.setChecked(true);
         } else {
             checkBox.setChecked(false);
         }
-
+        
+        //hide or show stars, checkboxes, trash cans based on whether delete, edit, or neither is selected
         if(deleteSelected && !EventActivity.editSelected){
             checkBox.setVisibility(View.VISIBLE);
             img_star.setVisibility(View.GONE);
@@ -85,11 +95,13 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
             img_edit.setVisibility(View.GONE);
             img_star.setVisibility(View.VISIBLE);
         }
-
+        
+        //detect when the current checkbox is toggled update dependent variables
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 int position = (int)buttonView.getTag();
+                
                 if(EventActivity.userSelection.contains(labels.get(position))){
                     EventActivity.userSelection.remove(labels.get(position));
                 }
@@ -98,13 +110,15 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                 }
             }
         });
-
+        
+         //detect when the current star is toggled and update listview appearance and dependent variables
         img_star.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String selected = labels.get(position_star);
                 final String selected_link = EventActivity.name2link.get(selected);
                 EventActivity.exportMap.clear();
+                
                 for(String x: EventActivity.EventData){
                     String local_key = x.split("~~")[0];
                     ArrayList<String> local_value = new ArrayList<>();
@@ -113,8 +127,10 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                     EventActivity.exportMap.put(local_key, local_value);
                     results.put(local_key, local_value);
                 }
+                
                 ArrayList<String> status = (ArrayList<String>) results.get(selected_link);
                 ArrayList<String> replacement = new ArrayList<>();
+                
                 if(status.get(1).equals("t")){
                     replacement.add(selected);
                     replacement.add("f");
@@ -129,17 +145,22 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                     favList.put(status.get(0), "t");
                     EventActivity.userSelectionFavorite.put(status.get(0), "t");
                 }
+                
                 EventActivity.EventData.clear();
+                
                 for (Map.Entry mapElement : results.entrySet()) {
                     String key = (String)mapElement.getKey();
                     ArrayList<String> val = (ArrayList<String>) mapElement.getValue();
                     String input_str = key + "~~" + val.get(0) + "~~" + val.get(1);
                     EventActivity.EventData.add(input_str);
                 }
+                
                 LoginActivity.rootRef.child(ListActivity.currentUserUID).child(EventActivity.selected_event).setValue(EventActivity.EventData);
                 notifyDataSetChanged();
             }
         });
+        
+        //detect when the pencil is selected and show the dialog box
         img_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -148,7 +169,8 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
         });
         return row;
     }
-
+    
+    //filter list items based on search query and update list
     public Filter getFilter() {
         return exampleFilter;
     }
@@ -156,6 +178,7 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
         @Override
         protected FilterResults performFiltering(CharSequence constraint) {
             List<String> filteredList = new ArrayList<>();
+            
             if(constraint == null || constraint.length() == 0) {
                 filteredList.addAll(labelsFull);
             } else {
@@ -166,6 +189,7 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                     }
                 }
             }
+            
             FilterResults results = new FilterResults();
             results.values = filteredList;
             return results;
@@ -177,6 +201,8 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
             notifyDataSetChanged();
         }
     };
+    
+    //handle input text for renaming the profile(make sure input text is valid & no duplicate profiles) & update listview and dependent variables
     private void showEditItemDialog(Context c, String clicked_name) {
         final EditText taskEditText = new EditText(c);
         taskEditText.setSingleLine(true);
@@ -190,6 +216,8 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     m_Text = String.valueOf(taskEditText.getText());
+                    
+                    //update dependent variables and listview
                     if(!m_Text.isEmpty()){
                         String prev_val_name2link = EventActivity.name2link.get(clicked_name);
                         EventActivity.name2link.put(m_Text, prev_val_name2link);
@@ -230,16 +258,21 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
             .create();
         dialog.show();
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+        
+        //make sure input text is valid and prevent duplicate profile names
         taskEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
+            
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+            
             @Override
             public void afterTextChanged(Editable s) {
                 dialog.setMessage(Html.fromHtml("<font color='#000000'>Give a new name: </font>"));
+                
                 if (TextUtils.isEmpty(s)) {
                     dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
                     return;
@@ -254,11 +287,13 @@ public class ListViewAdapter2 extends ArrayAdapter<String> {
                         taskEditText.setSelection(taskEditText.getText().length());
                         return;
                     }
+                    
                     if ((alphanum.indexOf(currentVal.charAt(currentLen - 1)) == -1) || isWhitespace(currentVal.charAt(0)) ||currentVal.contains("  ")) {
                         taskEditText.setText(currentVal.substring(0, currentVal.length() - 1));
                         taskEditText.setSelection(taskEditText.getText().length());
                         return;
                     }
+                    
                     if(EventActivity.names.contains(taskEditText.getText().toString()) || EventActivity.names.contains(taskEditText.getText().toString().trim()) || taskEditText.getText().toString().length() > 25){
                         if(EventActivity.names.contains(taskEditText.getText().toString()) || EventActivity.names.contains(taskEditText.getText().toString().trim())) {
                             dialog.setMessage(Html.fromHtml("<font color='#FF0000'>Profile Already Exist</font>"));
