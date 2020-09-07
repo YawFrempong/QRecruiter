c5a72dc6b4684e315be388edec84de5f1bcddc6d
Yaw Frempong <yfrempon@uncc.edu>
2020-09-07 15:30:47 -0400

    Update QRActivity.java

--- app/src/main/java/com/yfrempon/qrecruiter/QRActivity.java
+++ app/src/main/java/com/yfrempon/qrecruiter/QRActivity.java
@@ -32,9 +32,6 @@ import static android.Manifest.permission.CAMERA;
 import static java.lang.Character.isWhitespace;
 
 public class QRActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private static final int REQUEST_CAMERA = 1;
-    private ZXingScannerView scannerView;
-    private String m_Text = "";
     Intent intent;
     String passed_event;
     ArrayList<String> profiles;
@@ -42,6 +39,9 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
     ImageView btn_img;
     AnimatedVectorDrawableCompat avd;
     AnimatedVectorDrawable avd2;
+    private ZXingScannerView scannerView;
+    private String m_Text = "";
+    private static final int REQUEST_CAMERA = 1;
     private boolean paused = false;
 
     @Override
@@ -51,10 +51,15 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
         scannerView = findViewById(R.id.zxscan);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Scan QR Code");
+        
+        //get event and profile data passed in from the Event Activity
         intent = getIntent();
         passed_event = intent.getStringExtra("EVENT2");
         profiles = intent.getStringArrayListExtra("listOfNames");
+        
         temp = new HashMap<>();
+        
+        //handle camera access permissions
         int currentApiVersion = Build.VERSION.SDK_INT;
         if(currentApiVersion >=  Build.VERSION_CODES.M)
         {
@@ -64,14 +69,21 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             }
         }
     }
+    
+    //check if camera access has already been granted
     private boolean checkPermission()
     {
         return (ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA) == PackageManager.PERMISSION_GRANTED);
     }
+    
+    //ask for camera access permissions
     private void requestPermission()
     {
         ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
     }
+    
+    //AnimatedVectorDrawable requires API level 22 or higher
+    //start and stop camera: pause button -> play button | play button -> pause button
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void togglePlayButton(){
         paused = !paused;
@@ -92,6 +104,9 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             avd2.start();
         }
     }
+    
+    //AnimatedVectorDrawable requires API level 22 or higher
+    //initialize pause/play button
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void setTogglePlayButton(boolean val){
         if(val){
@@ -112,36 +127,8 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             avd2.start();
         }
     }
-    @Override
-    public void onResume() {
-        super.onResume();
-        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
-            if (checkPermission()) {
-                if(scannerView == null) {
-                    scannerView = new ZXingScannerView(this);
-                    setContentView(scannerView);
-                }
-                scannerView.setResultHandler(this);
-                scannerView.startCamera();
-            } else {
-                requestPermission();
-            }
-        }
-        btn_img = findViewById(R.id.pause_btn_img);
-        btn_img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                togglePlayButton();
-            }
-        });
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        scannerView.stopCamera();
-    }
-
+    
+    //handle camera request
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CAMERA:
@@ -158,7 +145,8 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
                 break;
         }
     }
-
+    
+    //show message box
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
         new androidx.appcompat.app.AlertDialog.Builder(QRActivity.this)
                 .setMessage(message)
@@ -167,11 +155,14 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
                 .create()
                 .show();
     }
-
+    
+    //triggered after a QR code is scanned
     @Override
     public void handleResult(Result result) {
         final String myResult = result.getText();
         String suggested = "";
+        
+        //make sure the generated URL is a LinkedIn profile
         if(myResult.contains("/in/") && myResult.contains("?")) {
             suggested = myResult.substring(myResult.indexOf("/in/") + 4, myResult.indexOf("?"));
         }
@@ -183,6 +174,8 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             setTogglePlayButton(true);
             return;
         }
+        
+        //parse username from URL
         if(suggested.contains("-")){
             String[] separated = suggested.split("-");
             String first_name = separated[0].substring(0, 1).toUpperCase() + separated[0].substring(1);
@@ -192,10 +185,12 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
         else{
             suggested = suggested.substring(0, 1).toUpperCase() + suggested.substring(1);
         }
+        
         final EditText taskEditText = new EditText(QRActivity.this);
         taskEditText.setSingleLine(true);
         taskEditText.setLines(1);
         taskEditText.setMaxLines(1);
+        
         Builder builder = new Builder(this);
         builder.setTitle("Scan Result");
         builder.setMessage("Add a name:");
@@ -210,6 +205,7 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             }
         });
         builder.setNegativeButton("Cancel", null);
+        
         suggested = suggested.replaceAll("/[^A-Za-z0-9]/", "");
         suggested = suggested.replaceAll("\\s+", " ");
         suggested = suggested.trim();
@@ -217,7 +213,8 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
         taskEditText.setSelection(taskEditText.getText().length());
         final AlertDialog alert1 = builder.create();
         alert1.show();
-
+        
+        //Check if profile has already been scanned & make sure initial input text for new profile name is valid
         if(profiles.contains(taskEditText.getText().toString()) || profiles.contains(taskEditText.getText().toString().trim())  || EventActivity.name2link.containsValue(myResult) || taskEditText.getText().toString().length() > 25){
             if(EventActivity.name2link.containsValue(myResult)){
                 String localKeyVal = "";
@@ -240,12 +237,15 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
         else{
             alert1.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(true);
         }
+        
         alert1.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(final DialogInterface arg0) {
                 setTogglePlayButton(true);
             }
         });
+        
+        //Check if profile has already been scanned & make sure edited input text for new profile name is valid
         taskEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
@@ -303,12 +303,16 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
             }
         });
     }
+    
+    //add profile
     private void addToServer(String event, String link, String name){
         EventActivity.EventData.add(link + "~~" + name + "~~f");
         profiles.add(name);
         EventActivity.name2link.put(name, link);
         LoginActivity.rootRef.child(ListActivity.currentUserUID).child(passed_event).setValue(EventActivity.EventData);
     }
+    
+    //back button on toolbar pressed
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -316,6 +320,39 @@ public class QRActivity extends AppCompatActivity implements ZXingScannerView.Re
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    //get current status of the camera when the activity resumes from being inactive(check permissions, listen of pause/play button clicks)
+    @Override
+    public void onResume() {
+        super.onResume();
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if(scannerView == null) {
+                    scannerView = new ZXingScannerView(this);
+                    setContentView(scannerView);
+                }
+                scannerView.setResultHandler(this);
+                scannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+        btn_img = findViewById(R.id.pause_btn_img);
+        btn_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                togglePlayButton();
+            }
+        });
+    }
+    
+    //stop camera when activity closes
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        scannerView.stopCamera();
     }
 }
 
